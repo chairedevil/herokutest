a17f75371cc8535b7e96b16f78ee94e366caaca8
chaiwat wattayakorn <chairedevil@live.com>
2018-10-02 12:41:21 +0900

    echoBot

--- echo_bot.php
+++ echo_bot.php
@@ -18,8 +18,8 @@
 
 require_once('./LINEBotTiny.php');
 
-$channelAccessToken = '<your channel access token>';
-$channelSecret = '<your channel secret>';
+$channelAccessToken = 'weCV1UB0jNHZOHIjosN0Cz5kCJ/mkiudeXUsYV7lBDtClTi3PmMDfhp9S3OMZJ3/K5fAtQFE0eE14KwVBojZKab9gqsurO81WYb7t73zvN1fRVdtfLuulXRxWH0441g1DfFZeb3Kdco5d6sfbABn1QdB04t89/1O/w1cDnyilFU=';
+$channelSecret = '2bc86a87dc5a71bf884791a3b52e67b8';
 
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 foreach ($client->parseEvents() as $event) {
