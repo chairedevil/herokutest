ba5bb100ac600258512455bea46a0454d203f340
chairedevil <chairedevil@live.com>
2018-10-02 21:27:27 +0900

    Update test.php

--- test.php
+++ test.php
@@ -20,7 +20,7 @@
 
     $cache = new Instagram\Storage\CacheManager('/path/to/your/cache/folder');
     $api   = new Instagram\Api($cache);
-    $api->setUserName('kunuthai');
+    $api->setUserName('chairedevil');
 
     $feed = $api->getFeed();
 
