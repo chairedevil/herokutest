a60c7771c3ee52a3a1175abeee96b35c966724fa
chaiwat wattayakorn <chairedevil@live.com>
2018-10-03 11:48:54 +0900

    1

--- echo_bot.php
+++ echo_bot.php
@@ -1,5 +1,6 @@
 <?php
 require_once('lib/LINEBotTiny.php');
+require_once('lib/replyMsg.class.php');
 
 $channelAccessToken = 'weCV1UB0jNHZOHIjosN0Cz5kCJ/mkiudeXUsYV7lBDtClTi3PmMDfhp9S3OMZJ3/K5fAtQFE0eE14KwVBojZKab9gqsurO81WYb7t73zvN1fRVdtfLuulXRxWH0441g1DfFZeb3Kdco5d6sfbABn1QdB04t89/1O/w1cDnyilFU=';
 $channelSecret = '2bc86a87dc5a71bf884791a3b52e67b8';
@@ -9,6 +10,7 @@ foreach ($client->parseEvents() as $event) {
     switch ($event['type']) {
         case 'message':
             $message = $event['message'];
+
             switch ($message['type']) {
                 case 'text':
                     $client->replyMessage(array(
@@ -16,7 +18,7 @@ foreach ($client->parseEvents() as $event) {
                         'messages' => array(
                             array(
                                 'type' => 'text',
-                                'text' => $tr->translate($message['text'])
+                                'text' => "chai"
                             )
                         )
                     ));
@@ -25,6 +27,7 @@ foreach ($client->parseEvents() as $event) {
                     error_log("Unsupporeted message type: " . $message['type']);
                     break;
             }
+
             break;
         default:
             error_log("Unsupporeted event type: " . $event['type']);
