c249bf4c20d52e276ec9b1e9e434dcb51fe0db27
chaiwat wattayakorn <chairedevil@live.com>
2018-10-03 12:44:06 +0900

    10

--- echo_bot.php
+++ echo_bot.php
@@ -19,6 +19,67 @@ foreach ($client->parseEvents() as $event) {
                         case '#':
                             $tranText = substr($message['text'], 1);
                             $sendMsg = $rm->translate($tranText);
+                            $sendObj = <?php
+require_once('lib/LINEBotTiny.php');
+require_once('lib/replyMsg.class.php');
+
+$channelAccessToken = 'rY8bFWWrNfgT7geI9PKV3LDb/stfZGW/NakTYGA4m1oaY0W1xvhUlvuSxtEUOWoGK5fAtQFE0eE14KwVBojZKab9gqsurO81WYb7t73zvN1UaAFjVmih0fLi8Nj/5J3ijFihgrg6Lh5vtvmz/RAaFQdB04t89/1O/w1cDnyilFU=';
+$channelSecret = '2bc86a87dc5a71bf884791a3b52e67b8';
+
+$client = new LINEBotTiny($channelAccessToken, $channelSecret);
+foreach ($client->parseEvents() as $event) {
+    switch ($event['type']) {
+        case 'message':
+            $message = $event['message'];
+
+            switch ($message['type']) {
+                case 'text':
+                    $inputText = $message['text'][0];
+                    $rm = new replyMsg();
+                    switch($inputText){
+                        case '#':
+                            $tranText = substr($message['text'], 1);
+                            $sendMsg = $rm->translate($tranText);
+                            $sendObj = array(
+                                'type' => 'text',
+                                'text' => $sendMsg
+                            );
+                            break;
+                        case '@':
+                            $userId = substr($message['text'], 1);
+                            $sendMsg = $rm->getLastInstra($userId);
+                            $sendObj = array(
+                                'type' => 'text',
+                                'text' => $sendMsg
+                            );
+                            break;
+                        default :
+                            $sendMsg = '分かった(' . $inputText . ') rev10';
+                            $sendObj = array(
+                                'type' => 'text',
+                                'text' => $sendMsg
+                            );
+                            break;
+                    }
+                    $client->replyMessage(array(
+                        'replyToken' => $event['replyToken'],
+                        'messages' => array(
+                            $sendObj
+                        )
+                    ));
+                    break;
+                default:
+                    error_log("Unsupporeted message type: " . $message['type']);
+                    break;
+            }
+
+            break;
+        default:
+            error_log("Unsupporeted event type: " . $event['type']);
+            break;
+    }
+};
+
                             break;
                         case '@':
                             $userId = substr($message['text'], 1);
