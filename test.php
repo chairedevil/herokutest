3dd55236f100ddc2b527687e4a76c84605119162
chaiwat wattayakorn <chairedevil@live.com>
2018-10-03 11:29:38 +0900

    rev14

--- test.php
+++ test.php
@@ -28,7 +28,7 @@
         if($message['type'] == "text"){
 
             echo $rm->gen($message['text']);
-            echo "<br>rev 13<br>";
+            echo "<br>rev 14<br>";
 
         }
 
@@ -55,7 +55,7 @@
             echo '============================' . "<br><br>";
             /** @var \Instagram\Hydrator\Component\Media $media */
             echo '<pre>';
-            print_r($feed->getMedias()[0]);
+            print_r($feed->getMedias()[0]['thumbnailSrc']);
             echo '</pre>';
                 //echo 'ID        : ' . $media->getId() . "<br>";
                 //echo 'Caption   : ' . $media->getCaption() . "<br>";
