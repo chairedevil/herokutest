644bc07706e99350b84576e49a146edb754f2702
chaiwat wattayakorn <chairedevil@live.com>
2018-10-03 10:19:52 +0900

    rev3

--- test.php
+++ test.php
@@ -5,28 +5,42 @@
     $message['type'] = "text";
     $message['text'] = "#こんばんは";
 
-    echo "<pre>";
-    print_r($message);
-    echo "</pre>";
+    //echo "<pre>";
+    //print_r($message);
+    //echo "</pre>";
 
     $rm = new replyMsg();
 
-    if($message['type'] == "text"){
-
-        echo $rm->gen($message['text']);
-
-    }
-
-
-
-    $cache = new Instagram\Storage\CacheManager('/path/to/your/cache/folder');
-    $api   = new Instagram\Api($cache);
-    $api->setUserName('pgrimaud');
-
-
-    $feed = $api->getFeed();
-
-    print_r($feed);
-
     //instagram Access Token : 1650237.6615052.2df86910def041519e66ed6cf97d15c8
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+        if($message['type'] == "text"){
+
+            echo $rm->gen($message['text']);
+            echo "<br>rev3<br>";
+
+        }
+
+        $cache = new Instagram\Storage\CacheManager('/path/to/your/cache/folder');
+        $api   = new Instagram\Api($cache);
+        $api->setUserName('pgrimaud');
+
+
+        $feed = $api->getFeed();
+
+        print_r($feed);
+
+    ?>
+</body>
+</html>
